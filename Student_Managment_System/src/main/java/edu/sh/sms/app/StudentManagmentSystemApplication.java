6dd12731e2961be70b41e3335762dbe1ada7ceed
snehahalnor107 <snehahalnor107@gmail.com>
2025-12-15 11:25:59 +0530

    spring boot student managment project.

---
+++ Student_Managment_System/src/main/java/edu/sh/sms/app/StudentManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.sh.sms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ Student_Managment_System/src/test/java/edu/sh/sms/app/StudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sh.sms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
